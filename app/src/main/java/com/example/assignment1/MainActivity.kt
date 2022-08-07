616f1f57e9a47e9af017c19562b4b3acca645d22
yuvika02 <trikhayuvika20@gmail.com>
2022-08-07 17:40:47 +0530

    login app

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.assignment1
 
-    import android.content.Intent
-    import androidx.appcompat.app.AppCompatActivity
-    import android.os.Bundle
-    import android.view.View
-    import android.widget.*
-    import androidx.core.widget.doOnTextChanged
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.core.widget.doOnTextChanged
+
 class MainActivity : AppCompatActivity() {
     lateinit var etName: EditText
     lateinit var etPassword: EditText
@@ -13,21 +14,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnLogin: Button
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      etName = findViewById(R.id.etName)
+        etName = findViewById(R.id.etName)
         etPassword = findViewById(R.id.etPassword)
 
         tvForgotPassword = findViewById(R.id.tvForgotPassword)
-        btnLogin = findViewById(R.id.btnLogin);
-
-                {
-                   var intent = Intent(this, logininfo ::class.java)
-                   startActivity(intent)
-               }
-
+        btnLogin = findViewById(R.id.btnLogin)
 
 
         etPassword.doOnTextChanged { text, _, _, _ ->
@@ -38,24 +32,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-            btnLogin.setOnClickListener {
-                etName.text.clear()
-                System.out.println("Clicked")
-                var name = etName.text.toString()
-                var password = etPassword.text.toString()
-                System.out.println("name $name")
-                if(name.isNullOrEmpty()){
-                    etName.error = resources.getString(R.string.please_enter_name)
-                    etName.requestFocus()
-                }else if(password.isNullOrEmpty()){
-                    etPassword.error = resources.getString(R.string.please_enter_password)
-                    etPassword.requestFocus()
-                }else if(password.length<6){
-                    etPassword.error = resources.getString(R.string.please_enter_password)
-                    etPassword.requestFocus()
-                }
-
+        btnLogin.setOnClickListener {
+            System.out.println("Clicked")
+            var name = etName.text.toString()
+            var password = etPassword.text.toString()
+            System.out.println("name $name")
+            if (name.isNullOrEmpty()) {
+                etName.error = resources.getString(R.string.please_enter_name)
+                etName.requestFocus()
+            } else if (password.isNullOrEmpty()) {
+                etPassword.error = resources.getString(R.string.please_enter_password)
+                etPassword.requestFocus()
+            } else if (password.length < 6) {
+                etPassword.error = resources.getString(R.string.please_enter_password)
+                etPassword.requestFocus()
+            } else {
+                var intent = Intent(this,activitylogin::class.java)
+                startActivity(intent)
             }
-        }
 
+        }
     }
+
+}
--- app/src/main/java/com/example/assignment1/logininfo.kt
+++ app/src/main/java/com/example/assignment1/logininfo.kt
@@ -10,78 +10,83 @@ import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 
-class logininfo : AppCompatActivity() {
+class activitylogin : AppCompatActivity() {
 
 
-lateinit var etName: EditText
-lateinit var etemail: EditText
-lateinit var etPhone: EditText
-lateinit var etpassword: EditText
+    lateinit var etName: EditText
+    lateinit var etemail: EditText
+    lateinit var etPhone: EditText
+    lateinit var etpassword: EditText
     lateinit var rgGender: RadioGroup
     lateinit var rbHe: RadioButton
     lateinit var rbShe: RadioButton
     lateinit var rbOther: RadioButton
     lateinit var etOtherName: EditText
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_login)
-    etName = findViewById(R.id.etName)
-    etemail = findViewById(R.id.etEmail)
-    etPhone = findViewById(R.id.etPhone)
-    etpassword = findViewById(R.id.etPassword)
-    rbHe = findViewById(R.id.rbHe)
-    rbShe = findViewById(R.id.rbShe)
-    rbOther = findViewById(R.id.rbOthers)
-    rgGender = findViewById(R.id.rgGender)
-    etOtherName = findViewById(R.id.etOtherName)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        etName = findViewById(R.id.etName)
+        etemail = findViewById(R.id.etEmail)
+        etPhone = findViewById(R.id.etPhone)
+        etpassword = findViewById(R.id.etPassword)
+        rbHe = findViewById(R.id.rbHe)
+        rbShe = findViewById(R.id.rbShe)
+        rbOther = findViewById(R.id.rbOthers)
+        rgGender = findViewById(R.id.rgGender)
+        etOtherName = findViewById(R.id.rbOtherName)
 
-    etName.doOnTextChanged { text, _, _, _ ->
-        if ((text?.length ?: 0) < 10) {
+        etName.doOnTextChanged { text, _, _, _ ->
+            if ((text?.length ?: 0) < 10) {
 
-            etName.error = resources.getString(R.string.enter_name)
+                etName.error = resources.getString(R.string.enter_name)
 
-        }}
+            }
+        }
         etemail.doOnTextChanged { text, _, _, _ ->
             if ((text?.length ?: 0) < 10) {
                 etemail.error = "Enter_email"
 
-            }}
-            etPhone.doOnTextChanged { text, _, _, _ ->
-                if ((text?.length ?: 0) < 10) {
-                    etPhone.error = "Enter_a_valid_Phone"
-                }}
-                etpassword.doOnTextChanged { text, _, _, _ ->
-                    if ((text?.length ?: 0) < 10) {
-                        etpassword.error = "Enter_password"
-
-                    }}
-    rbHe = findViewById(R.id.rbHe)
-    rbShe = findViewById(R.id.rbShe)
-    rbOther = findViewById(R.id.rbOthers)
-    rgGender = findViewById(R.id.rgGender)
-    rgGender.setOnCheckedChangeListener() { RadioGroup, id ->
-        when (id) {
-            R.id.rbOthers -> {
-                Toast.makeText(
-                    this,
-                    resources.getString(R.string.Others),
-                    Toast.LENGTH_LONG
-                ).show()
-                etOtherName.visibility = View.VISIBLE
             }
-            else -> {
+        }
+        etPhone.doOnTextChanged { text, _, _, _ ->
+            if ((text?.length ?: 0) < 10) {
+                etPhone.error = "Enter_a_valid_Phone"
+            }
+        }
+        etpassword.doOnTextChanged { text, _, _, _ ->
+            if ((text?.length ?: 0) < 10) {
+                etpassword.error = "Enter_password"
+
+            }
+        }
+        rbHe = findViewById(R.id.rbHe)
+        rbShe = findViewById(R.id.rbShe)
+        rbOther = findViewById(R.id.rbOthers)
+        rgGender = findViewById(R.id.rgGender)
+        rgGender.setOnCheckedChangeListener() { RadioGroup, id ->
+            when (id) {
+                R.id.rbOthers -> {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.Others),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    etOtherName.visibility = View.VISIBLE
+                }
+                else -> {
+                    etOtherName.visibility = View.INVISIBLE
+                }
+            }
+        }
+        rbOther.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                etOtherName.visibility = View.VISIBLE
+            } else {
                 etOtherName.visibility = View.INVISIBLE
             }
         }
     }
-    rbOther.setOnCheckedChangeListener { _, isChecked ->
-        if (isChecked) {
-            etOtherName.visibility = View.VISIBLE
-        } else {
-            etOtherName.visibility = View.INVISIBLE
-        }
-                }
-            }}
+}
 
 
 
